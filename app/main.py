31c13ee3d4807d703912b92b0733434e41ab7d95
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2024-12-09 22:22:30 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from fastapi import FastAPI, Response
 from routers import v1 
 app = FastAPI(
     title="Flapi",
-    version="1.0.0",
+    version="1.0.1",
     description="A free and open source api for devs to get data of their given api structure. it helps to make development faster without the headache of data collection.",
 )
 
